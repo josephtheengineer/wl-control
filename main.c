b9c1dca04142abac7fe292ac7d2418c9cd6ae5f6
josephtheengineer <josephtheengineer@pm.me>
2019-09-22 20:45:51 +1000

    Add basic server / client tcp connections

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
+// read mouse
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 
+// read keyboard
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -9,6 +11,17 @@
 #include <linux/input.h>
 #include <string.h>
 #include <stdio.h>
+
+// networking
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <time.h>
+#include <stdint.h>
 
 int read_mouse()
 {
@@ -88,9 +101,171 @@ int read_keyboard()
 	return EXIT_FAILURE;
 }
 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#define MAX 80 
+#define PORT 8080 
+#define SA struct sockaddr 
+
+// Function designed for chat between client and server. 
+void server_func(int sockfd) 
+{ 
+	char buff[MAX]; 
+	int n; 
+	// infinite loop for chat 
+	for (;;) { 
+		bzero(buff, MAX); 
+
+		// read the message from client and copy it in buffer 
+		read(sockfd, buff, sizeof(buff)); 
+		// print buffer which contains the client contents 
+		printf("From client: %s\t To client : ", buff); 
+		bzero(buff, MAX); 
+		n = 0; 
+		// copy server message in the buffer 
+		while ((buff[n++] = getchar()) != '\n') 
+			; 
+
+		// and send that buffer to client 
+		write(sockfd, buff, sizeof(buff)); 
+
+		// if msg contains "Exit" then server exit and chat ended. 
+		if (strncmp("exit", buff, 4) == 0) { 
+			printf("Server Exit...\n"); 
+			break; 
+		} 
+	} 
+} 
+
+// Driver function 
+int server_main() 
+{ 
+	int sockfd, connfd, len; 
+	struct sockaddr_in servaddr, cli; 
+
+	// socket create and verification 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1) { 
+		printf("socket creation failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully created..\n"); 
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	// assign IP, PORT 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+	servaddr.sin_port = htons(PORT); 
+
+	// Binding newly created socket to given IP and verification 
+	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+		printf("socket bind failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully binded..\n"); 
+
+	// Now server is ready to listen and verification 
+	if ((listen(sockfd, 5)) != 0) { 
+		printf("Listen failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Server listening..\n"); 
+	len = sizeof(cli); 
+
+	// Accept the data packet from client and verification 
+	connfd = accept(sockfd, (SA*)&cli, &len); 
+	if (connfd < 0) { 
+		printf("server acccept failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("server acccept the client...\n"); 
+
+	// Function for chatting between client and server 
+	server_func(connfd); 
+
+	// After chatting close the socket 
+	close(sockfd); 
+} 
+ 
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#define MAX 80 
+#define PORT 8080 
+#define SA struct sockaddr 
+void client_func(int sockfd) 
+{ 
+	char buff[MAX]; 
+	int n; 
+	for (;;) { 
+		bzero(buff, sizeof(buff)); 
+		printf("Enter the string : "); 
+		n = 0; 
+		while ((buff[n++] = getchar()) != '\n') 
+			; 
+		write(sockfd, buff, sizeof(buff)); 
+		bzero(buff, sizeof(buff)); 
+		read(sockfd, buff, sizeof(buff)); 
+		printf("From Server : %s", buff); 
+		if ((strncmp(buff, "exit", 4)) == 0) { 
+			printf("Client Exit...\n"); 
+			break; 
+		} 
+	} 
+} 
+
+int client_main() 
+{ 
+	int sockfd, connfd; 
+	struct sockaddr_in servaddr, cli; 
+
+	// socket create and varification 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1) { 
+		printf("socket creation failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully created..\n"); 
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	// assign IP, PORT 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+	servaddr.sin_port = htons(PORT); 
+
+	// connect the client socket to server socket 
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+		printf("connection with the server failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("connected to the server..\n"); 
+
+	// function for chat 
+	client_func(sockfd); 
+
+	// close the socket 
+	close(sockfd); 
+} 
+
 int main(int argc, char** argv)
 {
 	//read_mouse();
-	read_keyboard();
+	//read_keyboard();
+	//setup_client();
+	//setup_server();
+	//server_main();
+	client_main();
 	return 0;
 }
